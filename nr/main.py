93ca27b28d7741c54fcc3d53d82b59c32a2632e2
tanto <kqqk1234@gmail.com>
2023-05-28 00:27:55 +0900

    remove version

--- nr/main.py
+++ nr/main.py
@@ -1,20 +1,11 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from datetime import datetime
-from .utils import git_info
 from .routers import root, new_releases
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Server received the first request!")
-
-    app.start_time=datetime.utcnow()
-    app.version=git_info.get_tag_or_branch()
-    app.commit=git_info.get_revision()
-    app.dirty=git_info.get_dirty()
-
     yield
-
     print("Server received SIGTERM signal.")
 
 app = FastAPI(
--- nr/routers/root.py
+++ nr/routers/root.py
@@ -1,16 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 
 router = APIRouter()
 
 @router.get("/ping")
 def ping():
     return {"ping": "pong"}
-
-@router.get("/version")
-def version(request: Request):
-    return {
-        "version": request.app.version,
-        "commit": request.app.commit,
-        "start_time": request.app.start_time,
-        "dirty": request.app.dirty
-    }
