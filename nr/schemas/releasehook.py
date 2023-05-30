f9e9eb34439c9236f91c1d518d4b047d02e6b9c6
tanto <kqqk1234@gmail.com>
2023-05-30 20:38:07 +0900

    fix webhook dto

--- nr/schemas/releasehook.py
+++ nr/schemas/releasehook.py
@@ -1,14 +1,19 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Optional, List
 
 
 class Note(BaseModel):
-    title: str
-    message: str
+    title: Optional[str]
+    message: Optional[str]
 
 
 class Account(BaseModel):
     _type: Field(str, alias="type")
+    _id: Field(str, alias="id")
+    name: str
+
+class Tag(BaseModel):
     _id: Field(str, alias="id")
     name: str
 
@@ -18,10 +23,13 @@ class ReleaseHook(BaseModel):
     project: str
     version: str
     time: datetime
-    is_prerelease: bool
-    is_updated: bool
-    note: Note
+    cve: Optional[List[str]]
+    is_prerelease: Optional[bool]
+    is_updated: Optional[bool]
+    note: Optional[Note]
     account: Account
+    tags: Optional[Tag]
+    project_note: Optional[str]
 
     class Config:
         schema_extra = {
